2502523f1972ac0e394d103369ad71a8bd917ad2
Alvin Jose <alvjose@publicisgroupe.net>
2023-01-24 19:43:30 +0530

    decorator

---
+++ Decorator.ts
@@ -0,0 +1,22 @@
+function invoke(constructor:Function) {
+    // the new constructor behaviour
+   const newconstructor: any = function(...args) {
+      this.productId = 875;
+      this.productName = 'Tablet';
+      };
+  // return newconstructor will override the original constructor
+   return newconstructor;
+ }
+@invoke
+class Product {
+    public productId: number;
+    public productName: string;
+    constructor(productId: number, productName: string) {
+        this.productId = productId;
+        this.productName = productName;
+        }
+}
+const p = new Product(326, 'Mobile');
+console.log(`Product id is: ${p.productId}`);
+console.log(`Product name is : ${p.productName}`);
+ 
